fbd1ac1145b8eacc932675d5905ca183f431a145
ashim8 <ashimshil54321@gmail.com>
2022-05-23 22:23:07 +0600

    header added

--- src/App.js
+++ src/App.js
@@ -2,11 +2,13 @@
 import { Route, Routes } from 'react-router-dom';
 import './App.css';
 import Home from './components/Home/Home/Home';
+import Header from './components/Shared/Header/Header';
 
 
 function App() {
   return (
     <div className="App">
+      <Header></Header>
       <Routes>
         <Route path='/' element={<Home></Home>}></Route>
         {/* <Route path='product/:id' element={}></Route> */}
