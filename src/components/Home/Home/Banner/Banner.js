5018776a8c84849871d082b3473495d7d96c49fd
ashim8 <ashimshil54321@gmail.com>
2022-05-23 23:40:34 +0600

    banner added

---
+++ src/components/Home/Home/Banner/Banner.js
@@ -0,0 +1,29 @@
+import React, { useState } from 'react';
+import { Carousel } from 'react-bootstrap';
+
+import banner1 from '../../../../images/banner1.jpg';
+
+const Banner = () => {
+    const [index, setIndex] = useState(0);
+
+  const handleSelect = (selectedIndex, e) => {
+    setIndex(selectedIndex);
+  };
+    return (
+        <Carousel activeIndex={index} onSelect={handleSelect}>
+            <Carousel.Item>
+                <img
+                    className="d-block w-100"
+                    src={banner1}
+                    alt="First slide"
+                />
+                <Carousel.Caption>
+                    <h3>First slide label</h3>
+                    <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+                </Carousel.Caption>
+            </Carousel.Item>
+        </Carousel>
+    );
+};
+
+export default Banner;
